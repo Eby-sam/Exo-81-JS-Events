9367107fbbfe1230c9892139d2e2c7814641847f
coque <coquelet.samuel@gmail.com>
2021-11-18 13:35:14 +0100

    fini

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,7 +1,8 @@
 /* Modifier le code ci dessous */
 
-document.getElementById("cliquezmoi").("", function(){
+document.getElementById("cliquezmoi").addEventListener("click", function(){
     alert('Click')
 });
+
 
 
